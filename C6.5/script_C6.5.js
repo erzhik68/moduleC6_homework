60783f4ac9656543a4ad6910feee3773b2790437
erzhik68 <93429091+erzhik68@users.noreply.github.com>
2022-03-11 14:14:01 +0500

    Add files via upload

---
+++ C6.5/script_C6.5.js
@@ -0,0 +1,7 @@
+﻿const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  const width = window.screen.width;
+  const height = window.screen.height;
+  alert(`Размеры экрана: ширина - ${width}, высота - ${height}`);
+});
